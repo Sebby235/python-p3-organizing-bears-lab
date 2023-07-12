ec3c64f4e34a2f21a7191978500455c4c0e1c5d7
Sebastian Martinez <isaacsoccer235@gmail.com>
2023-07-12 06:34:14 -0500

    sql

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 28, "M", "Brown", "Nice", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 1, "F", "Light Brown", "Hyper", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 5, "F", "White", "Chill", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 25, "M", "Dark Brown", "Stern", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 20, "F", "Black", "Mean", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 10, "M", "Green", "Angry", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 12, "F", "Black", "Shy", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, "M", "Dark Brown", "Sad", True);
